c3b1bf5bda6765d5625f60ce259862852696f1c9
Amery2010 <amery@xiangfa.org>
2026-03-12 18:00:39 +0800

    build: Enables on-demand loading and does not bundle external dependencies.

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,46 @@
-import { dirname, resolve } from "node:path";
+import { dirname, extname, relative, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
+import { glob } from "glob";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 const dir = dirname(fileURLToPath(import.meta.url));
+
+// 收集所有入口文件，排除 spec 测试文件
+const entries = Object.fromEntries(
+  glob
+    .sync("src/editor/**/*.ts", {
+      ignore: ["src/**/*.spec.ts"],
+    })
+    .map((file) => [
+      relative("src", file.slice(0, file.length - extname(file).length)),
+      resolve(dir, file),
+    ]),
+);
+
 export default defineConfig({
+  plugins: [
+    dts({
+      include: ["src/editor"],
+      exclude: ["src/**/*.spec.ts", "src/main.ts", "src/settings.ts"],
+    }),
+  ],
   build: {
-    cssCodeSplit: true,
     lib: {
-      entry: resolve(dir, "src/editor", "index.ts"),
-      name: `markdown_editor`,
-      fileName: (format: string) => `index.${format}.js`,
-      formats: ["es", "cjs"],
+      entry: resolve(dir, "src/editor/index.ts"),
+      formats: ["es"],
     },
+    minify: false,
     rollupOptions: {
-      output: [
-        {
-          dir: resolve(dir, "lib/esm"),
-          format: "esm",
-        },
-        {
-          dir: resolve(dir, "lib/cjs"),
-          format: "cjs",
-        },
-      ],
+      external: [/^@codemirror\//, /^@lezer\//, /^codemirror/, /^lucide/],
+      input: entries,
+      output: {
+        format: "esm",
+        dir: resolve(dir, "dist"),
+        preserveModules: true,
+        preserveModulesRoot: "src",
+        entryFileNames: "[name].js",
+      },
     },
   },
 });
